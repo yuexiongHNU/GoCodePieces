1fef0ce3517abf6fb96d19255571b514bd831aa4
YueXiong <xiongyue_hnu@163.com>
2018-03-26 15:32:00 +0800

    Update BmizeranyPQMain.go

--- postgresql/BmizeranyPQMain.go
+++ postgresql/BmizeranyPQMain.go
@@ -4,14 +4,14 @@ import (
 )
 
 func main() {
-	db := newMyDB("liujiedb", "liujieuser", "kingsoftliujie", "120.92.176.250", 5432)
+	db := newMyDB("dbname", "username", "password", "host", 5432)
 	c := db.newConn()
 	defer c.Close()
 	db.createSchema("go_test", c )
 	db.createTable("go_test", "userinfo", c)
 	data_map := map[string]string {
 		"username": "yuexiong",
-		"departname": "BaoLi",
+		"departname": "haoli",
 		// "created": "2018-03-25",
 		// time.Now().Format("2006-01-21")
 	}
