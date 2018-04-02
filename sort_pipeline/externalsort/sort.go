12314e04e94988253c9758f33c83293693aa9c75
YueXiong <xiongyue_hnu@163.com>
2018-04-02 09:05:35 +0800

    Add files via upload

--- sort_pipeline/externalsort/sort.go
+++ sort_pipeline/externalsort/sort.go
@@ -1,1 +1,60 @@
+package main
 
+import (
+	"os"
+	"HelloWorld/pipeline"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	p := createPipeline("small.in", 512, 4)
+	writeToFile(p, "small.out")
+	printFile("small.out")
+}
+
+func createPipeline(filename string, fileSize int, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+
+	sortResults := []<-chan int{}
+	for i:=0; i< chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i * chunkSize), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+
+		sortResults = append(sortResults, pipeline.InMemSort(source))
+	}
+
+	return pipeline.MergeN(sortResults...)
+}
+
+func writeToFile(p <-chan int, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	pipeline.WriterSink(writer, p)
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.ReaderSource(file, -1)
+	for v := range p {
+		fmt.Println(v)
+	}
+}
